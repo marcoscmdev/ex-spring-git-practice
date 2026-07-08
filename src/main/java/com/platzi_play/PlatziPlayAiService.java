8505d176f81fae75371437e7f6fcdac84f6e51c1
MarcosCM <castromirandamarcos@gmail.com>
2026-07-08 13:18:59 +0200

    trackeo archivos

---
+++ src/main/java/com/platzi_play/PlatziPlayAiService.java
@@ -0,0 +1,12 @@
+package com.platzi_play;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface PlatziPlayAiService {
+    @UserMessage("""
+            Genera un saludo de bienvenida a la plataforma del Curso de Gestión de peliculas Platzi-Play. Usa menos de 120
+            caracteres y hazlo con el estilo de Platzi""")
+     String generateGretting();
+}
