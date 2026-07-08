fc098a7c673a5563f1f4625bb2ed57e58a5b1a34
MarcosCM <castromirandamarcos@gmail.com>
2026-07-08 14:26:08 +0200

    Cambio a 80 caracteres en curso

--- src/main/java/com/platzi_play/PlatziPlayAiService.java
+++ src/main/java/com/platzi_play/PlatziPlayAiService.java
@@ -6,7 +6,7 @@ import dev.langchain4j.service.spring.AiService;
 @AiService
 public interface PlatziPlayAiService {
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma del Curso de Gestión de peliculas Platzi-Play. Usa menos de 120
-            caracteres y hazlo con el estilo de Platzi""")
+            Genera un saludo de bienvenida a la plataforma del Curso de Gestión de peliculas Platzi-Play. Usa menos de 80
+                                                      caracteres y hazlo con el estilo de Platzi""")
      String generateGretting();
 }
