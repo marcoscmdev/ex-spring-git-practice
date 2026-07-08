d354e62ba9f6745054b5974ab0ff44e6f560ac96
MarcosCM <castromirandamarcos@gmail.com>
2026-07-08 14:16:01 +0200

    Cambio texto para provocar conflicto

--- src/main/java/com/platzi_play/PlatziPlayAiService.java
+++ src/main/java/com/platzi_play/PlatziPlayAiService.java
@@ -7,6 +7,6 @@ import dev.langchain4j.service.spring.AiService;
 public interface PlatziPlayAiService {
     @UserMessage("""
             Genera un saludo de bienvenida a la plataforma del Curso de Gestión de peliculas Platzi-Play. Usa menos de 120
-            caracteres y hazlo con el estilo de Platzi""")
+            caracteres y hazlo con el estilo de Platzi, son unos buenos flipados""")
      String generateGretting();
 }
