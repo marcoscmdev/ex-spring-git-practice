72096fecf97e34894c908b20160228454eab9a29
MarcosCM <castromirandamarcos@gmail.com>
2026-07-14 12:49:45 +0200

    Clase crear endpoint get, añado propiedad de ordenacion para el JSON

--- src/main/java/com/platzi_play/web/controllers/MovieController.java
+++ src/main/java/com/platzi_play/web/controllers/MovieController.java
@@ -1,4 +1,23 @@
 package com.platzi_play.web.controllers;
 
+import com.platzi_play.persistance.crud.CrudMovieEntity;
+import com.platzi_play.persistance.entity.MovieEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class MovieController {
+
+    private final CrudMovieEntity crudMovieEntity;
+
+    public MovieController(CrudMovieEntity crudMovieEntity) {
+        this.crudMovieEntity = crudMovieEntity;
+    }
+
+    @GetMapping("/movies")
+    public List<MovieEntity> getAll(){
+        return (List<MovieEntity>) this.crudMovieEntity.findAll();
+    }
 }
